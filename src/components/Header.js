e5000681a8cb055f1777f7d81a086a2efb29927e
JacquelineHockin <jacquelinehcashman@gmail.com>
2023-01-09 23:20:01 -1000

    adding and deleting code to app.js,header.js,index.js,index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from "react";
 import Nav from "./Nav";
 
 function Header(props) {
-  const { currentTab, handleTabCHange } = props;
+  const { currentTab, handleTabChange } = props;
 
   return (
     <section id="header" className="header">
-      <Nav currentTab={currentTab} handleTabCHange={handleTabCHange}></Nav>
+      <Nav currentTab={currentTab} handleTabChange={handleTabChange}></Nav>
       <h1>Software Developer</h1>
     </section>
   );
