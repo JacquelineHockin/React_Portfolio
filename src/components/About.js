1f1ee3363f847488999d157758e47f751e1f9584
JacquelineHockin <jacquelinehcashman@gmail.com>
2023-02-21 17:11:07 -1000

    adds to css

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,19 @@ import React from "react";
 
 function About() {
   return (
-    <section id="aboutMe" className="">
-      <h1>About Me</h1>
+    <section id="aboutMe" className="my-5 intro">
+      <p class="me1"></p>
+
+      <div class=" flex-row wrapper">
+        <div class="intro-img">
+          <img id="profpic" src="profpic.jpg" alt="prof-pic" />
+          <p class="me">
+          Hello! My name is Jacqueline. I am a health/wellness practitioner and community activist building apps to do a lil somethin' good. 
+        I'm interested in working with individuals and teams that have a philanthropic lens. I find that I'm the most driven and creative
+        when connected to the 'why'. Please don't hesitate to reach out if there's a project you believe I'd be a good addition to. 
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
